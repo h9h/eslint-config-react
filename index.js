970a30ec740e86598ac4bb4add3aec5135d8db8a
Michael (at Home) <dev@h9h.de>
2018-11-18 23:33:48 +0100

    adaptiert

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ module.exports = {
     "prettier/prettier": "error"
   },
   extends: ["prettier"],
-  parser: "babel-eslint"
+  parser: "babel-eslint",
+  prettier: {
+    printWidth: 80,
+    parser: "flow",
+    semi: false,
+    singleQuote: true
+  }
 };
